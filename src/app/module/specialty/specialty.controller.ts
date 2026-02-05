6dbb39de5430f6448090e16f8a18bd4a8276e5d9
shazid25 <ishazid57@gmail.com>
2026-02-05 21:51:52 +0600

    created API for create, get & delete Soecialties

---
+++ src/app/module/specialty/specialty.controller.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from "express";
+import { SpecialtyService } from "./specialty.service.";
+
+const createSpecialty = async (req: Request , res: Response)=>{
+   try {
+     const payload = req.body;
+
+    const result = await SpecialtyService.createSpecialty(payload);
+
+    res.status(201).json({
+        success: true,
+        message: 'Spcialty created successfully',
+        data: result
+    });
+   } catch (error:any) {
+    console.log(error);
+    res.status(500).json({
+        success: false,
+        message: 'Failed to create specilty',
+        error: error.message
+    });
+   }
+}
+
+const getAllSpecialties = async (req: Request, res: Response)=>{
+try {
+    const specialties = await SpecialtyService.getAllSpecialties();
+
+    res.status(200).json({
+        success: true,
+        message: 'Specialties fetched successfully',
+        data: specialties
+    });
+    
+}  catch (error:any) {
+    console.log(error);
+    res.status(500).json({
+        success: false,
+        message: 'Failed to create specilty',
+        error: error.message
+    });
+   }
+}
+
+
+const deleteSpecialty = async(req:Request, res: Response)=>{
+    try {
+        const { id } = req.params;
+
+        const result = await SpecialtyService.deleteSpecialty(id as string);
+
+         res.status(200).json({
+        success: true,
+        message: 'Specialties fetched successfully',
+        data: result
+    });
+        
+    } catch (error:any) {
+    console.log(error);
+    res.status(500).json({
+        success: false,
+        message: 'Failed to delete specilty',
+        error: error.message
+    });
+   }
+}
+
+export const SpecialtyController = {
+    createSpecialty,
+    getAllSpecialties,
+    deleteSpecialty
+}
