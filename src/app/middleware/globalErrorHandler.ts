732382a4a3946299b0ec98249035df29a8bec829
shazid25 <ishazid57@gmail.com>
2026-03-09 01:23:19 +0600

    Creating Zod Error Handler For Global Error Handler

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,14 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../../config/env";
 import status from "http-status";
+import z from "zod";
+
+interface TErrorSources {
+    path: string;
+    message: string;
+}
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, _next: NextFunction) =>{
    if(envVars.NODE_ENV === 'development'){
         console.log("Error from global error handler: ", err);
    }
+      
+   const errorSource: TErrorSources[] = [];
 
-   const statusCode: number = status.INTERNAL_SERVER_ERROR;
-   const message : string = 'Internal Server Error';
+   let statusCode: number = status.INTERNAL_SERVER_ERROR;
+   let message : string = 'Internal Server Error';
+
+   if(err instanceof z.ZodError){
+    statusCode = status.BAD_REQUEST;
+    message = "Zod validation error";
+   }
 
     res.status(statusCode).json({
         success: false,
